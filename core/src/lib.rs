5791327a0bc94d47afa53604ffc92516e85f2439
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-17 10:10:16 -0500

    adding arg parsing and a response obj

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::env;
+
+pub struct Response {
+    pub message: String,
+    pub exit_code: i32,
+}
+
+pub struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +40,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_response() {
+        let response = Response {
+            message: "test".to_string(),
+            exit_code: 0,
+        };
+        assert_eq!(response.exit_code, 0);
     }
 }
--- swan/src/main.rs
+++ swan/src/main.rs
@@ -1,3 +1,16 @@
+use core::{Config, Response};
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let sample_response = Response {
+        message: "Hello, World!".to_string(),
+        exit_code: 0,
+    };
+    println!("{}", sample_response.message);
 }
