f4ece11e2146e5a4229472424a57b3fd787ba012
Ryan Swanson <ryan.s.swanson@gmail.com>
2023-01-22 12:09:12 -0500

    wip - working on help command data structures

---
+++ subcommands/swan-help/src/commands.rs
@@ -0,0 +1,31 @@
+use std::{collections::HashMap, error::Error};
+
+pub struct Command {
+    command: &'static str,
+    pub help_message: &'static str,
+}
+
+pub const help: Command = Command {
+    command: "help",
+    help_message:  "usage: swan <command> <subcommand>",
+};
+
+pub const about: Command = Command {
+    command: "about",
+    help_message: "usage: swan about",
+};
+
+pub const creds: Command = Command {
+    command: "creds",
+    help_message: "usage: swan creds <cloud_provider>",
+};
+
+pub fn get_command_list<'a>() -> Box<HashMap<&'static str, Command>> { 
+    let mut commands = HashMap::new();
+    
+    commands.insert(help.command, help);
+    commands.insert(about.command, about);
+    commands.insert(creds.command, creds);
+
+    Box::new(commands)
+}
